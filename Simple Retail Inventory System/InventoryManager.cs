27105b90964de92ec5336f71fe1aa5f4ae661184
Ej <elnesjanisidoro416@gmail.com>
2025-01-31 11:58:32 +0800

    created inventory manager class with addProduct function

---
+++ Simple Retail Inventory System/InventoryManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Retail_Inventory_System
+{
+    internal class InventoryManager
+    {
+        Dictionary<int, Product> inventory= new Dictionary<int, Product>();
+
+        public void AddProduct(int id,Product product) {
+            inventory.Add(id,product);
+        }
+
+        public void Show() {
+            for (int i = 0; i < inventory.Count; i++)
+            {
+                Console.WriteLine($"\n{inventory[i]}\n");
+            }
+        }
+    }
+}
--- Simple Retail Inventory System/Program.cs
+++ Simple Retail Inventory System/Program.cs
@@ -10,23 +10,39 @@ namespace Simple_Retail_Inventory_System
     {
         static void Main(string[] args)
         {
-            Product product = CreateProduct();
-            Console.WriteLine(product);
 
+            InventoryManager inventoryManager = new InventoryManager();
+            int inventoryId = 0;
+            bool isLooping = true;
+
+            while (isLooping)
+            {
+               
+                inventoryManager.AddProduct(inventoryId, CreateProduct(inventoryId));
+            }
+
+            inventoryManager.Show();
             Console.ReadLine();
         }
 
         // product data creation
-        static Product CreateProduct()
+        static Product CreateProduct(int id)
         {
             Product product = new Product
             {
-                ProductId = 1,
+                ProductId = id,
                 ProductName = "Test",
                 ProductPrice = 10,
                 ProductQuantity = 100,
             };
             return product;
         }
+
+        //helper function for getting user input
+        static string UserInput(string msg) {
+            Console.Write(msg);
+            string input = Console.ReadLine();
+            return input;
+        } 
     }
 }

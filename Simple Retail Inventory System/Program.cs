c1a1f7b1728daf198896e9eb38e98ec3cf5dd060
Ej <elnesjanisidoro416@gmail.com>
2025-01-31 11:00:38 +0800

    Add project files.

---
+++ Simple Retail Inventory System/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Retail_Inventory_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            Console.ReadLine();
+        }
+    }
+}
